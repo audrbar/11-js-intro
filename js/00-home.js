638aedd1c8184f22764f1c49fa9d8bf6df101da7
audrbar <audrbar@gmail.com>
2022-12-13 21:50:38 +0200

    homework

---
+++ js/00-home.js
@@ -0,0 +1,25 @@
+console.log('----kintamuju iniciavimas-----')
+const as = 1;
+const bs = 2;
+const cs = 3;
+console.log('Kintamieji su skaiciaus tipo reiksmemis: ', as, bs, cs);
+
+const at = 'tekstas';
+const bt = 'tekstui';
+const ct = 'teksto';
+console.log('Kintamieji su testo tipo reiksmemis: ', at, bt, ct);
+
+const buildings = [25, 35, 27, 45, 68];
+const postCodes = [05213, 25871, 36524, 25698, 01254];
+const flatsNo = [56, 58, 47, 23, 45];
+console.log('Building Numbers: ', buildings);
+console.log('Post Codes: ', postCodes);
+console.log('Flats Numbers: ', flatsNo);
+
+const fruits = ['apple', 'orange', 'blueberry', 'lemon', 'kiwi'];
+const vegetables = ['carrot', 'onion', 'potato', 'cabbage', 'pumpkin'];
+const nuts = ['greek', 'coconut', 'walnut', 'hazelnut', 'nutmeg'];
+console.log('Fruits: ', fruits);
+console.log('Vegetables: ', vegetables);
+console.log('Nuts: ', nuts);
+console.log('----Veiksmai su kintamaisiais----');
