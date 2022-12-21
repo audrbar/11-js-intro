8deccab6cc8972b0c7549b5b2677c22f8938d563
audrbar <102819309+audrbar@users.noreply.github.com>
2022-12-21 13:26:28 +0200

    spread, rest

---
+++ js/14-spread.js
@@ -0,0 +1,103 @@
+console.clear();
+
+// destrukturizavimas - destructuring
+
+const pazymiai = [10, 9, 8, 7, 6];
+const pirmas = pazymiai[0];
+const antras = pazymiai[1];
+
+console.log(pirmas);
+console.log(antras);
+console.log('----');
+const marks = [10, 2, 8, 4, 6];
+const [first, third] = marks;
+
+console.log(first);
+console.log(third);
+console.log('----');
+
+const petras = ['Petras', 99, true];
+const vardas = petras[0];
+const amzius = petras[1];
+const arVedes = petras[2];
+console.log(vardas, amzius, arVedes);
+
+const ona = ['Ona', 87, false];
+const [name, age, isMarried] = ona;
+console.log(name, age, isMarried);
+
+const car = {
+    brand: 'Tesla',
+    model: 'S',
+    color: 'red'
+}
+const { color, model } = car;
+console.log(color, model);
+
+// SPREAD
+
+const numbers = [1, 2, 3];
+const numbers2 = [...numbers, ...numbers];
+const numbers3 = [...numbers2, 999, ...numbers2];
+
+console.log(numbers);
+console.log(numbers2);
+console.log(numbers3);
+
+const person = {
+    name: 'Maryte',
+    age: 88,
+    isMarried: true,
+    childrenCount: 5,
+    phone: 2148562548,
+    address: 'Gatves g 5, Miestas',
+    favoriteColor: 'yellow',
+    height: 200,
+    eyes: '👀'
+}
+console.log(person);
+
+const person2 = {
+    ...person,
+    car: {
+        ...car,
+        price: 100000
+    },
+    happy: true,
+}
+
+console.log(person2);
+console.log(person2.car.brand);
+
+const { childrenCount, happy, favoriteColor } = person2;
+console.log(childrenCount, happy, favoriteColor);
+/* Footer
+© 2022 GitHub, Inc.
+Footer navigation
+Terms
+Privacy
+Security
+Status
+Docs
+*/
+
+// Spread - israsymas, iskopijavimas
+console.clear();
+
+const a = ['a', 'aa'];
+const b = ['b', 'bb'];
+const c = ['c', 'cc'];
+
+const doubleABC = [...a, 111, ...b, ...c];
+const doubleCBA = [...c, ...b, 222, ...a];
+console.log(doubleABC);
+console.log(doubleCBA);
+
+const oABC = { ...a, gg: 'gg', cc: 'nebe CC' };
+
+
+const user = {
+    name: 'Username',
+    password: 'pasword',
+};
+console.log(user);
---
+++ js/15-rest.js
@@ -0,0 +1,74 @@
+console.clear();
+// Rest operator - surenka likusias nepaimtas reiksmes destrukt8rizuojant
+
+const marks = [10, 2, 8, 4, 6];
+const [first, second, ...visiKiti] = marks;
+
+console.log(marks);
+console.log(first);
+console.log(second);
+console.log(visiKiti);
+
+console.log('--------------------------------');
+const person = {
+    name: 'Petras',
+    isMarried: true,
+    favorite: {
+        color: 'red',
+        number: 13,
+        artist: 'Spice Girls'
+    }
+}
+const { age, isMarried, ...other } = person;
+console.log(age, isMarried);
+console.log(other);
+
+console.log('--------Du parametrai---------');
+
+function sum(a, b) {
+    return a + b;
+}
+
+const s1 = sum(7, 5);
+console.log(s1);
+
+console.log('--------Vienas parametras---------');
+
+function arraySum(list) {
+    let totalSum = 0;
+
+    for (let i = 0; i <list.lenght; i++) {
+        totalSum += list[i];
+    }
+
+    return totalSum;
+}
+
+const s2 = arraySum([10, 2, 8, 4, 6]);
+console.log(s2);
+console.log('--------Kitas---------');
+function sudetis(...args) {
+    let total = 0;
+
+    for (let i = 0; i <args.lenght; i++) {
+        totalSum += args[i];
+    }
+
+    return total;
+}
+
+const s3 = sudetis(10, 2, 8, 4, 6);
+console.log(s3);
+
+console.log('--------Dar kitas---------');
+function product() {
+    let total = 1;
+    for (let i = 0; i <[...arguments].lenght; i++) {
+        totalSum *= [...arguments][i];
+    }
+
+    return total;
+}
+
+const s4 = product(10, 2, 8, 4, 6);
+console.log(s4);
