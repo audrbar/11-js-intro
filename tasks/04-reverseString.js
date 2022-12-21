2f61033d870afbde9143b6575c38bcd31d276c7a
audrbar <102819309+audrbar@users.noreply.github.com>
2022-12-21 10:55:17 +0200

    reverseString

---
+++ tasks/04-reverseString.js
@@ -0,0 +1,12 @@
+console.clear();
+/*
+Revers revrite text
+*/
+
+function reverseString(str) {
+    return str;
+}
+
+console.log(reverseString('asd'), '-->', 'dsa');
+console.log(reverseString('ffff'), '-->', 'ffff');
+console.log(reverseString('sula'), '-->', 'alus');
